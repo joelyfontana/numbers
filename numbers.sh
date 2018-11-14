d9d43616944768749629814c26861195817736ea
Joely Fontana <fontana@chapman.edu>
2018-11-14 11:59:44 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#numbers.sh
+#Joely Fontana
+
+echo "Enter a positive number: "
+read num
+n=1
+
+while [[ $n -le num ]]
+do
+	if [[ $((n%2)) -eq 0 ]]
+	then 
+		echo "$n is even"
+	else	 
+		echo "$n is odd"
+	fi
+	n=$[$n+1]	
+done
+
